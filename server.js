84e930c47ed967d2dd950df9007efc254f92c866
hereiam123 <bdemaio1@gmail.com>
2019-07-28 21:59:16 -0400

    Setting up server port

--- server.js
+++ server.js
@@ -1,36 +1,36 @@
-global.fetch = require("node-fetch");
-const config = require("universal-config");
-const Unsplash = require("unsplash-js").default;
-const toJson = require("unsplash-js").toJson;
-const express = require("express");
-const path = require("path");
-const dotenv = require("dotenv");
+global.fetch = require('node-fetch');
+const config = require('universal-config');
+const Unsplash = require('unsplash-js').default;
+const toJson = require('unsplash-js').toJson;
+const express = require('express');
+const path = require('path');
+const dotenv = require('dotenv');
 
 dotenv.config({
-  path: "./.env"
+  path: './.env'
 });
 
 const unsplash = new Unsplash({
-  applicationId: config.get("APPLICATION_ID") || process.env.APPLICATION_ID,
-  secret: config.get("SECRET") || process.env.SECRET,
-  callbackUrl: config.get("CALLBACK_URL") || process.env.CALLBACK_URL
+  applicationId: config.get('APPLICATION_ID') || process.env.APPLICATION_ID,
+  secret: config.get('SECRET') || process.env.SECRET,
+  callbackUrl: config.get('CALLBACK_URL') || process.env.CALLBACK_URL
 });
 
 const app = express();
 
-app.get("/api/photos", (req, res) => {
+app.get('/api/photos', (req, res) => {
   unsplash.photos
     .listPhotos(req.query.start, req.query.count)
     .then(toJson)
     .then(json => res.json(json));
 });
 
-app.use(express.static(path.join(__dirname + "/client/build/")));
+app.use(express.static(path.join(__dirname + '/client/build/')));
 
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
